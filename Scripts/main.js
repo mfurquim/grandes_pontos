1d0009a3d99b9b1aa2c973a2249e9598b9d21a09
Mateus Furquim <furquim.axs@gmail.com>
2015-09-09 12:42:02 -0300

    Adjusting the window size.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,54 +1,24 @@
 var canvas = null,
-    context = null,
-    atlasJSON = null,
-    url = "./Assets/Assets.png";
+		context = null,
+		atlasAsset = null,
+		url = "./Assets/Assets.png";
 
 function load() {
-  context.fillText('Loading', 152, 152);
-
-  context.drawImage(atlasJSON, 0, 0);
-  context.drawImage(atlasJSON, 2, 2, 42, 28, 150, 400, 42, 28);
-
-  context.fillText('Game Loaded', 152, 200);
-}
-
-function cropAndDraw(filename, destinationX, destinationY) {
-  var sourceX = 0,
-      sourceY = 0,
-      width = 0,
-      height = 0;
-
-  function parseAtlas(atlJSON) {
-    var parsed = JSON.parse(atlJSON);
-    console.log("Oi");
-
-    for(var key in parsed.frames) {
-      var sprite = parsed.frames[key];
-
-      console.log(sprite);
-
-//    this.defAtlas(key, sprite.frame.x, sprite.frame.y, sprite.frame.w, sprite.frame.h, cx, cy);
-    }
-  }
-
-  parseAtlas(atlasJSON);
-
+	context.drawImage(atlasAsset, 2, 2, 42, 28, 150, 400, 42, 28);
 }
 
 var setup = function() {
-  canvas = document.getElementById("myCanvas");
-  context = canvas.getContext("2d");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+	canvas = document.getElementById("myCanvas");
+	context = canvas.getContext("2d");
+	canvas.width = (window.innerWidth - 20);
+	canvas.height = (window.innerHeight - 20);
 
-  context.font = '40px Arial';
-  context.fillStyle = 'green';
+	context.font = '40px Arial';
+	context.fillStyle = 'green';
 
-  atlasJSON = new Image();
-  atlasJSON.onload = load;
-  atlasJSON.src = url;
-
-  cropAndDraw("Bordered/classic_blue_.png", 150, 150);
+	atlasAsset = new Image();
+	atlasAsset.onload = load;
+	atlasAsset.src = url;
 };
 
 setup();
