e108c2c12749398fa492855a4af63e0542d9bf3c
matmello <mat.mello93@gmail.com>
2015-10-08 21:29:13 -0300

    generate Board

--- Scripts/main.js
+++ Scripts/main.js
@@ -8,10 +8,61 @@ var setup = function() {
 	context = canvas.getContext("2d");
 	canvas.width = (window.innerWidth - offsetWidth);
 	canvas.height = (window.innerHeight - offsetHeight);
+	generateBoard();
 };
 
 setup();
 ATLAS.loadingAssets();
 
 // It needs to wait a little for the sprites to be loaded
-window.setTimeout(ATLAS.drawSprite, 100, "Multi/disc_green.png", 0, 0);
+// Generate Board
+
+
+
+function generateBoard() {
+var board=[];
+var currentDisc;
+for (var discX = 0;discX<10;discX++){
+	board[discX]=[];
+	for (var discY = 0; discY<10;discY++) {
+		console.log(discX,discY);
+
+	board[discX].push(Math.floor(Math.random()*(7 - 1 + 1))+1);
+
+	currentDisc = drawDisc(board[discX][discY]);
+	posX = discX * 50;
+	posY = discY * 50;
+
+	window.setTimeout(ATLAS.drawSprite, 100, currentDisc, posX, posY);
+
+	}
+}
+console.log("********");
+console.log(board);
+}
+
+function drawDisc(discNumber) {
+	var disc;
+	if(discNumber==1){
+		disc = "Multi/disc_green.png";
+	}
+	else if (discNumber==2){
+		disc = "Multi/disc_blue.png";
+	}
+	else if (discNumber==3){
+		disc = "Multi/disc_red.png";
+	}
+	else if (discNumber==4){
+		disc = "Multi/disc_purple.png";
+	}
+	else if (discNumber==5){
+		disc = "Multi/disc_yellow.png";
+	}
+	else if (discNumber==6){
+		disc = "Multi/disc_white.png";
+	}
+	else if (discNumber==7){
+		disc = "Multi/disc_black.png";
+	}
+	return disc;
+}
