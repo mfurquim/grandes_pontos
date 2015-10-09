3aa1f8c8a005e2882a722f019cba6e6cee2ab825
matmello <mat.mello93@gmail.com>
2015-10-08 23:20:20 -0300

    creating game board

--- Scripts/main.js
+++ Scripts/main.js
@@ -20,49 +20,90 @@ ATLAS.loadingAssets();
 
 
 function generateBoard() {
+var discCount=[];
+
+for(var i = 0; i<=6; i++){
+	discCount[i]=0;
+}
+
 var board=[];
 var currentDisc;
-for (var discX = 0;discX<10;discX++){
+for (var discX = 0;discX<11;discX++){
 	board[discX]=[];
-	for (var discY = 0; discY<10;discY++) {
-		console.log(discX,discY);
+	for (var discY = 0; discY<5;discY++) {
+		// console.log(discX,discY);
 
-	board[discX].push(Math.floor(Math.random()*(7 - 1 + 1))+1);
+ 	discNumber = Math.floor(Math.random()*(7));
+
+	discCount[discNumber]+=1;
+
+	// console.log(discCount[discNumber]);
+
+
+	if(validateDisc(discCount,discNumber)){
+	board[discX].push(discNumber);
 
 	currentDisc = drawDisc(board[discX][discY]);
 	posX = discX * 50;
 	posY = discY * 50;
 
 	window.setTimeout(ATLAS.drawSprite, 100, currentDisc, posX, posY);
+}
+else {
 
+discY-=1;
+
+
+discCount[discNumber]-=1;
+}
 	}
 }
-console.log("********");
-console.log(board);
+// console.log("********");
+// console.log(board);
 }
 
 function drawDisc(discNumber) {
 	var disc;
-	if(discNumber==1){
+	if(discNumber===0){
 		disc = "Multi/disc_green.png";
 	}
-	else if (discNumber==2){
+	else if (discNumber==1){
 		disc = "Multi/disc_blue.png";
 	}
-	else if (discNumber==3){
+	else if (discNumber==2){
 		disc = "Multi/disc_red.png";
 	}
-	else if (discNumber==4){
+	else if (discNumber==3){
 		disc = "Multi/disc_purple.png";
 	}
-	else if (discNumber==5){
+	else if (discNumber==4){
 		disc = "Multi/disc_yellow.png";
 	}
-	else if (discNumber==6){
+	else if (discNumber==5){
 		disc = "Multi/disc_white.png";
 	}
-	else if (discNumber==7){
+	else if (discNumber==6){
 		disc = "Multi/disc_black.png";
 	}
 	return disc;
+}
+
+function validateDisc(discCount,discNumber){
+	// console.log("validando");
+	// console.log(discCount[discNumber]);
+	var isValid = true;
+	if (discCount[discNumber]>9){
+		isValid = false;
+	}
+	else if (discNumber==5 && discCount[5]>5){
+		isValid = false;
+	}
+	else if (discNumber==6 && discCount[6]>5){
+		isValid = false;
+	}
+	else {
+		//
+	}
+	// console.log(isValid);
+	return isValid;
 }
