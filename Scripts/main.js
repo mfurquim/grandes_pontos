04af2dc7c77d19f9e7a136394163e9116369b8d9
Mateus Furquim <furquim.axs@gmail.com>
2015-10-18 11:23:35 -0200

    Refactoring events.js and adding flag to debug information.

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,6 +3,9 @@ var canvas       = null,
 		offsetWidth  = 20,
 		offsetHeight = 20;
 
+// If the debug flag is on, print debug information to console
+const DEBUG_ON = false;
+
 var setup = function() {
 	canvas = document.getElementById("myCanvas");
 	context = canvas.getContext("2d");
