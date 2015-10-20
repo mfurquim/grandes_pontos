f69fa48e9d00cf4ed443fded21d36893c3ad67aa
Mateus Furquim <furquim.axs@gmail.com>
2015-10-20 14:37:58 -0200

    Adding method to change sprite name (and make it by combining the arguments properly).

--- Scripts/gameObject.js
+++ Scripts/gameObject.js
@@ -19,19 +19,22 @@
  * Sprite as defined in Atlas.js
  * positionCoordinates (x,y)
  * scale (width, height)
+ *
  */
 var GameObject = function (sprite, positionCoordinates, scale) {
 	this._sprite = {};
+	
 	try {
 		this._sprite.name = sprite.name;
 		this._sprite.sourceCoordinates = sprite.sourceCoordinates;
 		this._sprite.dimensions = sprite.dimensions;
-		this._sprite.positionCoordinates = positionCoordinates;
-		this._sprite.scale = scale;
 	}
 	catch (errorSprite) {
 		alert(errorSprite);
 	}
+
+	this._positionCoordinates = positionCoordinates;
+	this._sprite.scale = scale;
 };
 
 /**
@@ -47,8 +50,8 @@ GameObject.prototype.drawItself = function(context, atlasImage) {
 		this._sprite.sourceCoordinates.y,
 		this._sprite.dimensions.width,
 		this._sprite.dimensions.height,
-		this._sprite.positionCoordinates.x,
-		this._sprite.positionCoordinates.y,
+		this._positionCoordinates.x,
+		this._positionCoordinates.y,
 		this._sprite.dimensions.width * this._sprite.scale.width,
 		this._sprite.dimensions.height * this._sprite.scale.height);
 }
@@ -71,4 +74,40 @@ GameObject.prototype.move = function(positionCoordinates) {
  */
 GameObject.prototype.scale = function(scale) {
 	this._sprite.scale = scale;
+}
+
+/**
+ * changeName changes the sprite name of the GameObject
+ *
+ * @param
+ * name (border/type_color.png)
+ */
+GameObject.prototype.changeName = function(border, type, color) {
+	var newName = this.makeName(border, type, color);
+	console.log(newName);
+	var newSprite = ATLAS.fetchSprite(newName);
+	try {
+		this._sprite.name = newSprite.name;
+		this._sprite.sourceCoordinates = newSprite.sourceCoordinates;
+		this._sprite.dimensions = newSprite.dimensions;
+	}
+	catch (errorSprite) {
+		alert(errorSprite);
+	}
+}
+
+/**
+ * makeName combines parameters into appropriate form to change sprite's name
+ *
+ * @param
+ * border (multi, single or bordered)
+ * type (disc, pawn1, pawn2, pawn3, classic, thin, human or meeple)
+ * color (green, blue, red, purple, yellow, white or black)
+ *
+ * @return
+ * name (border/type_color.png)
+ */
+GameObject.prototype.makeName = function(border, type, color) {
+	var newName = border + "/" + type + "_" + color + ".png";
+	return newName;
 }
