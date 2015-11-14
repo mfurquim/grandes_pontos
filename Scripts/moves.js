e1eb933299b1fc84786f4bb7c7185605e27f4c31
matmello <mat.mello93@gmail.com>
2015-11-14 16:08:59 -0200

    updating moves

--- Scripts/moves.js
+++ Scripts/moves.js
@@ -24,14 +24,41 @@ function nextPossibleMove (hoveredPawn){
     if (board.discs[i]._color == pawnColor) {
       //console.log(pawnColor," ### ", board.discs[i]._color);
 
+
+
       var nextCoordinate = {
         x : board.discs[i]._positionCoordinates.x,
         y : board.discs[i]._positionCoordinates.y
       };
 
 /** Draw Function **/
+    var color;
+
+    switch (board.discs[i]._color) {
+      case 0: color = "green";
+      break;
+      case 1: color = "blue";
+      break;
+      case 2: color = "red";
+      break;
+      case 3: color = "purple";
+      break;
+      case 4: color = "yellow";
+      break;
+      case 5: color = "white";
+      break;
+      case 6: color = "black";
+      break;
+    }
+
+
+
+    board.discs[i].changeName("Bordered","disc",color);
+
+
 
       break;
     }
   }
+  
 }
