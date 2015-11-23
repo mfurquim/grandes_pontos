2cd5084b3fc330a422179dab9655b190ef441531
Mateus Furquim <furquim.axs@gmail.com>
2015-11-23 10:53:08 -0200

    Moving in zig-zag (as it is supposed to do)

--- Scripts/moves.js
+++ Scripts/moves.js
@@ -19,38 +19,51 @@ function nextPossibleMove (hoveredPawn){
 
   for (var j = NUMBER_DISC_COL; j > 0; j--) {
     for (var i = NUMBER_DISC_ROW; i > 0; i--) {
+/*
       var num = ((i-1)*5)+(j-1);
-      console.log("(" + String(i) + "," + String(j) + ") -> " + String(num));
+*/
+      var num = 0;
+      if (j % 2 === 0) {
+        num = ((NUMBER_DISC_ROW-i)*5)+(j-1);
+        console.log("Par");
+        console.log("(" + String(i) + "," + String(j) + ") -> " + String(num));
+      } else {
+        num = ((i-1)*5)+(j-1);
+        console.log("Impar");
+        console.log("(" + String(i) + "," + String(j) + ") -> " + String(num));
+      }
+//      console.log("(" + String(i) + "," + String(j) + ") -> " + String(num));
       currentDisc = board.discs[num];
-      if (currentDisc._color == pawnColor) {
+      if (currentDisc._color === pawnColor) {
         var nextCoordinate = {
           x : currentDisc._positionCoordinates.x,
           y : currentDisc._positionCoordinates.y
         };
 
-      /** Draw Function **/
-      var color;
+        /** Draw Function **/
+        var color;
 
-      switch (currentDisc._color) {
-        case 0: color = "green";
-        break;
-        case 1: color = "blue";
-        break;
-        case 2: color = "red";
-        break;
-        case 3: color = "purple";
-        break;
-        case 4: color = "yellow";
-        break;
-        case 5: color = "white";
-        break;
-        case 6: color = "black";
-        break;
-      }
+        switch (currentDisc._color) {
+          case 0: color = "green";
+          break;
+          case 1: color = "blue";
+          break;
+          case 2: color = "red";
+          break;
+          case 3: color = "purple";
+          break;
+          case 4: color = "yellow";
+          break;
+          case 5: color = "white";
+          break;
+          case 6: color = "black";
+          break;
+        }
 
-      //console.log(currentDisc);
-      currentDisc.changeName("Bordered","disc",color);
+        //console.log(currentDisc);
+        currentDisc.changeName("Bordered","disc",color);
 
+        console.log("(" + String(i) + "," + String(j) + ") -> " + String(num));
         return currentDisc;
       }
     }
