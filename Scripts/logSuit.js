e704327d11fe224f814f62fbe7173a50ed217b10
matmello <mat.mello93@gmail.com>
2015-11-30 09:07:09 -0200

    simplifing and changing log functions

--- Scripts/logSuit.js
+++ Scripts/logSuit.js
@@ -1,41 +1,34 @@
-
-function createCookie(name, value, expires, path, domain) {
-  var cookie = name + "=" + escape(value) + ";";
-  if (expires) {
-     // If it's a date
-     if(expires instanceof Date) {
-       // If it isn't a valid date
-       if (isNaN(expires.getTime()))
-        expires = new Date();
-     }
-     else
-       expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60 * 60 * 24);
-
-     cookie += "expires=" + expires.toGMTString() + ";";
-   }
-
-   if (path)
-     cookie += "path=" + path + ";";
-   if (domain)
-     cookie += "domain=" + domain + ";";
-
-   document.cookie = cookie;
+function createCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    expires = "; expires="+date.toGMTString();
+  }
+  else {
+  expires = "";
+}
+  document.cookie = name+"="+value+expires+"; path=/";
 }
 
-function getCookie(name) {
-  var regexp = new RegExp("(?:^" + name + "|;\s*"+ name + ")=(.*?)(?:;|$)", "g");
-  var result = regexp.exec(document.cookie);
-  return (result === null) ? null : result[1];
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
 }
 
-function deleteCookie(name, path, domain) {
-  // If the cookie exists
-  if (getCookie(name))
-    createCookie(name, "", -1, path, domain);
+function eraseCookie(name) {
+  createCookie(name,"",-1);
 }
+
 
 createCookie("author", "aurelio", 30);
 createCookie("author", "asdasd", 30);
 
-console.log(getCookie("author")); // print aurelio
-console.log(getCookie("nothing")); // print null
+console.log(readCookie("author")); // print aurelio
+console.log(readCookie("nothing")); // print null
